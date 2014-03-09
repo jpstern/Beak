d0a5b023757e93fdb20974fa65d755cbdaf9cfc6
Josh Stern <jpstern@umich.edu>
2014-03-09 19:28:33 -0400

    Initial Commit

---
+++ Beak/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Beak
+//
+//  Created by Josh Stern on 3/9/14.
+//  Copyright (c) 2014 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Beak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Beak
+//
+//  Created by Josh Stern on 3/9/14.
+//  Copyright (c) 2014 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
